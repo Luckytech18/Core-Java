d830ee4d313d653935cd57661b3c20ebc51324ee
Luckytech18 <Luckytomar1801@gamil.com>
2025-08-12 18:15:07 +0530

    oky

---
+++ Java-oop/src/com/rays/oop/TestAccount.java
@@ -0,0 +1,41 @@
+package com.rays.oop;
+
+public class TestAccount {
+
+	public static void main(String[] args) {
+		 
+		Account a = new Account();
+		
+		a.setAccNumber("123456789");
+		a.setAccType("saving");
+		a.setBalance(10000.0);
+		
+		System.out.println("account no: " + a.getAccNumber());
+		System.out.println("account type: " + a.getAcctype());
+		System.out.println("account balance:" + a.getBalance());
+		
+		a.deposit(100.0);
+		a.withdrawal(9000.0);
+		
+		
+		System.out.println("-------------------------------------");
+		
+		Account a1 = new Account();
+		
+		a1.setAccNumber("6555826684");
+		a1.setAccType("current");
+		a1.setBalance(25000.5);
+		
+		
+		System.out.println("account no: " + a1.getAccNumber());
+		System.out.println("account type: " + a1.getAcctype());
+		System.out.println("account balance:" + a1.getBalance());
+		
+		a1.deposit(500.0);
+		a1.withdrawal(60000);
+		
+		
+	}
+	
+	
+}
---
+++ Java-oop/src/com/rays/oop/TestPerson.java
@@ -0,0 +1,23 @@
+package com.rays.oop;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class TestPerson {
+ public static void main(String[] args) throws ParseException {
+	
+	 SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	 
+	 Person p = new Person();
+	 
+	 p.setName("Lucky");
+	 p.setDob(sdf.parse("2005-01-18"));
+	 p.setAddress("Gwalior");
+	 
+	 System.out.println("Name:"+p.getName());
+	 System.out.println("Dob:"+p.getDob());
+	 System.out.println("Address"+ p.getAddress());
+	
+			 
+}
+}
